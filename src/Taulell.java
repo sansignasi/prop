1643be994fc901ee35214d300b1fd30637aa32f4
Ignasi <sans.ignasi@gmail.com>
2019-03-14 17:16:33 +0100

    Ignasi

--- src/Taulell.java
+++ src/Taulell.java
@@ -1,6 +1,14 @@
 package src;
 
 public class Taulell {
-    //matriu de 8*8 de peces
-    //
+    //ATRIBUTS
+    //Peça [][] matriu = new Peça[8][8];
+        //Matriu d'objectes Peça ([i][j] = null si en la posició i,j no hi ha cap Peça)
+    https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+    //MÈTODES
+    //mostrarTaulell(matrix);
+        //PRE:
+        //POST: mostra l'estat actual de les peces al taulell
+    //actualitzarTaulell(Peça p1, pos p)
+        //PRE: p1 és una peça
 }
