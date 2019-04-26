003d5f6208aba22bc10a35e7dda0d55fcf80d6ed
Ignasi <sans.ignasi@gmail.com>
2019-04-26 12:20:31 +0200

    Ignasi

--- src/TaulellTest.java
+++ src/TaulellTest.java
@@ -1,50 +1,99 @@
 package src;
-/*
+import lib.Pair;
+
+import org.junit.jupiter.api.BeforeAll;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaulellTest {
 
     @org.junit.jupiter.api.Test
-    void copiaPiece() {
+    void carregaFEN() throws IncorrectFENException {
+        Taulell t = new Taulell();
+        t.carregaFEN("3k2Q1/8/3K4/8/8/8/8/8 w - - 0 1");
+        /*matriu equivalent al FEN = { {null, null, null,new King('b',0,3),null,null,new Queen('w',0,6),null},
+                {null,null,null,null,null,null,null,null},
+                {null,null,null,new King('w',2,3),null,null,null,null},
+                {null,null,null,null,null,null,null,null},
+                {null,null,null,null,null,null,null,null},
+                {null,null,null,null,null,null,null,null},
+                {null,null,null,null,null,null,null,null},
+                {null,null,null,null,null,null,null,null}};*/
+
+        assertNull(t.getTaulell()[0][0]);
+        assertNull(t.getTaulell()[5][5]);
+        assertEquals(t.getTaulell()[0][3].getTipus(),"King");
+        assertEquals(t.getTaulell()[0][3].getColor(),'b');
+        assertEquals(t.getTaulell()[0][6].getTipus(),"Queen");
+        assertEquals(t.getTaulell()[0][6].getColor(),'w');
+        assertEquals(t.getTaulell()[2][3].getTipus(),"King");
+        assertEquals(t.getTaulell()[2][3].getColor(),'w');
     }
 
     @org.junit.jupiter.api.Test
-    void copiaTaulell() {
+    void copiaPiece() throws IncorrectFENException{
+        Taulell t = new Taulell();
+        t.carregaFEN("3k2Q1/8/3K4/8/8/8/8/8 w - - 0 1");
+        Piece px = t.copiaPiece(t.getPiece(0,3));
+        assertEquals(px.getTipus(),t.getPiece(0,3).getTipus());
+        assertEquals(px.getColor(),t.getPiece(0,3).getColor());
     }
 
     @org.junit.jupiter.api.Test
-    void getTaulell() {
+    void copiaTaulell() throws IncorrectFENException{
+        Taulell t = new Taulell();
+        t.carregaFEN("3k2Q1/8/3K4/8/8/8/8/8 w - - 0 1");
+        Taulell tx = new Taulell();
+        tx.copiaTaulell(t);
+        assertEquals(tx.getPiece(0,3).getTipus(),t.getPiece(0,3).getTipus());
+        assertEquals(tx.getPiece(0,3).getColor(),t.getPiece(0,3).getColor());
     }
 
     @org.junit.jupiter.api.Test
-    void getPiece() {
+    void actualitzarTaulell() throws IncorrectFENException{
+        Taulell t = new Taulell();
+        t.carregaFEN("3k2Q1/8/3K4/8/8/8/8/8 w - - 0 1");
+        t.actualitzarTaulell(t.getPiece(0,3),new Pair(0,0));
+        assertEquals(t.tePiece(0,0),true);
+        assertEquals(t.getPiece(0,0).getTipus(),"King");
     }
 
     @org.junit.jupiter.api.Test
-    void actualitzarTaulell() {
+    void tePiece() throws IncorrectFENException{
+        Taulell t = new Taulell();
+        t.carregaFEN("3k2Q1/8/3K4/8/8/8/8/8 w - - 0 1");
+        assertEquals(t.tePiece(0,3),true);
+        assertEquals(t.tePiece(7,6),false);
     }
 
     @org.junit.jupiter.api.Test
-    void tePiece() {
+    void teRei() throws IncorrectFENException {
+        Taulell t = new Taulell();
+        t.carregaFEN("3k2Q1/8/3R4/8/8/8/8/8 w - - 0 1");
+        assertEquals(t.teRei(1),true);
+        assertEquals(t.teRei(0),false);
     }
 
     @org.junit.jupiter.api.Test
-    void teRei() {
+    void taulellAFEN() throws IncorrectFENException{
+        Taulell t = new Taulell();
+        t.carregaFEN("3k2Q1/8/3K4/8/8/8/8/8 w - - 0 1");
+        assertEquals(t.taulellAFEN(),"3k2Q1/8/3K4/8/8/8/8/8");
     }
 
     @org.junit.jupiter.api.Test
-    void carregaFEN() {
+    void jaque() throws IncorrectFENException{
+        Taulell t = new Taulell();
+        t.carregaFEN("3k2Q1/8/8/8/8/8/8/8 w - - 0 1");
+        assertEquals(t.jaque(1),true);
+        assertEquals(t.jaque(0),false);
     }
 
     @org.junit.jupiter.api.Test
-    void taulellAFEN() {
+    void jaquemate() throws IncorrectFENException{
+        Taulell t = new Taulell();
+        t.carregaFEN("3k2Q1/8/3K4/8/8/8/8/8 w - - 0 1");
+        assertEquals(t.jaque(1),true);
+        assertEquals(t.jaque(0),false);
     }
-
-    @org.junit.jupiter.api.Test
-    void mostrarTaulell() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void jaquemate() {
-    }
-}*/
+}
