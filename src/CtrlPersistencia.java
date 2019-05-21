96a1e210867390b5d482712b55a22b63c48c961c
lop1498 <polgarciarecasens@gmail.com>
2019-05-21 11:01:44 +0200

    pol

---
+++ src/CtrlPersistencia.java
@@ -0,0 +1,10 @@
+package src;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CtrlPersistencia {
+
+        //aqui aniran tots els gets
+        //usuari
+        //problema
+}
